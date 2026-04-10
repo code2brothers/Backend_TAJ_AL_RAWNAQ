5d297591a37a93ae9625c4bb178e4a69bda1c73e
Suraj Kumar <kumarsuraj841504@gmail.com>
2026-04-10 23:44:12 +0530

    refactor: remove commented-out code from ApiResponse class for clarity

--- src/utils/ApiResponse.ts
+++ src/utils/ApiResponse.ts
@@ -9,22 +9,4 @@ class ApiResponse<T = any> {
     }
 }
 
-// it auto add to this which come in constructor
-// class ApiResponse{
-//
-//     public statusCode
-//     public message
-//     public data
-//     public success
-//     constructor(statusCode:number , data:any, message="Success") {
-//           this.statusCode = statusCode
-//           this.data = data
-//           this.message = message
-//           this.success = statusCode<400
-//     }
-// }
-
-
-
-
 export {ApiResponse}
